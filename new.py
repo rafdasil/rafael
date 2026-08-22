be31f66d705f6519371290fdcca196a5917a2cce
rafdasil <rdasilvarodrigues7@gmail.com>
2026-08-22 00:07:58 -0300

    Update print statements and add new messages
    "codigo alterado pelo site /p maquina local"

--- new.py
+++ new.py
@@ -1,4 +1,8 @@
-print("Hello world!")
-print("Meu nome é rafael")
-print("Tenho 20 anos")
-print("Estou aprendendo Python")
+#print("Hello world!")
+#print("Meu nome é rafael")
+#print("Tenho 20 anos")
+#print("Estou aprendendo Python")
+print("Olá mundo !")
+print("Alteração bem sucedida no site!")
+print("Baixando para o not")
+print("depois testar outras coisas !!! ")#não esquece pf
